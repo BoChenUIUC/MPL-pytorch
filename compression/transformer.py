54276d077546e781eaaef5a523f8bbe0a50d39f9
Bo Chen <bochen1993cs@gmail.com>
2021-03-10 16:17:42 -0600

    check shape

--- compression/transformer.py
+++ compression/transformer.py
@@ -313,6 +313,7 @@ class Transformer:
 
 	def transform(self, image=None, C_param=None):
 		self.original_size += image.shape[0]*image.shape[1]
+		print(image.shape)
 		rimage,comp_sz = tile_disturber(image, C_param)
 		self.compressed_size += comp_sz
 		return rimage
