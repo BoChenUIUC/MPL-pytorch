1babf92ffe72c49316e9a4a2ee320c7d00354e9b
Bo Chen <bochen1993cs@gmail.com>
2021-04-21 10:22:57 -0500

    test

--- app.py
+++ app.py
@@ -385,12 +385,12 @@ def deepcod_main(param,datarange):
             origin_labels,origin_features = disc_model(images_norm,True)
 
             reg_loss = orthorgonal_regularizer(gen_model.sample.weight,0.1,args.device != 'cpu')
-            # loss += criterion_mse(images,recon)
+            recon_loss = criterion_mse(images,recon)
             # loss += criterion_ce(recon_labels, targets)
             feat_loss = 0
             for origin_feat,recon_feat in zip(origin_features,recon_features):
                 feat_loss += criterion_mse(origin_feat,recon_feat)
-            loss = reg_loss + feat_loss
+            loss = reg_loss + recon_loss + feat_loss
                     
             optimizer.zero_grad()
             loss.backward()
@@ -401,8 +401,9 @@ def deepcod_main(param,datarange):
             top5.update(acc5[0], targets.shape[0])
             train_iter.set_description(
                 f"Train: {epoch:3}. "
-                f"top1: {top1.avg:.2f}. top5: {top5.avg:.2f}. loss: {loss.cpu().item():.6f}. "
-                f"reg_loss: {reg_loss.cpu().item():.6f}. feat_loss: {feat_loss.cpu().item():.6f}. ")
+                f"top1: {top1.avg:.2f}. top5: {top5.avg:.2f}. loss: {loss.cpu().item():.3f}. "
+                f"reg_loss: {reg_loss.cpu().item():.6f}. feat_loss: {feat_loss.cpu().item():.3f}. "
+                f"recon_loss: {recon_loss.cpu().item():.3f}. ")
 
         train_iter.close()
 
@@ -427,10 +428,11 @@ def deepcod_main(param,datarange):
                 _,origin_features = disc_model(images_norm,True)
 
                 reg_loss = orthorgonal_regularizer(gen_model.sample.weight,0.1,args.device != 'cpu')
+                recon_loss = criterion_mse(images,recon)
                 feat_loss = 0
                 for origin_feat,recon_feat in zip(origin_features,recon_features):
                     feat_loss += criterion_mse(origin_feat,recon_feat)
-                loss = reg_loss + feat_loss
+                loss = reg_loss + recon_loss + feat_loss
 
                 acc1, acc5 = accuracy(outputs, targets, (1, 5))
                 top1.update(acc1[0], targets.shape[0])
@@ -438,7 +440,8 @@ def deepcod_main(param,datarange):
                 test_iter.set_description(
                     f"Test: {epoch:3}. "
                     f"top1: {top1.avg:.2f}. top5: {top5.avg:.2f}. loss: {loss.cpu().item():.6f}. "
-                    f"reg_loss: {reg_loss.cpu().item():.6f}. feat_loss: {feat_loss.cpu().item():.6f}. ")
+                    f"reg_loss: {reg_loss.cpu().item():.6f}. feat_loss: {feat_loss.cpu().item():.6f}. "
+                    f"recon_loss: {recon_loss.cpu().item():.3f}. ")
 
         test_iter.close()
         torch.save(gen_model.state_dict(), PATH)
