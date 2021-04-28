751a1abe5714990a93f1f91abc381bf39b67e751
Bo Chen <bochen1993cs@gmail.com>
2021-04-28 08:59:08 -0500

    test

--- ccve.py
+++ ccve.py
@@ -582,9 +582,9 @@ def dual_train(net):
 		torch.save(net.state_dict(), PATH)
 
 def test():
-	from app import deepcod_main,deepcod_validate
+	from app import deepcod_main,deepcod_validate2
 	# deepcod_main(np.array([-0.1,0,-0.3]),None)
-	deepcod_validate()
+	deepcod_validate2()
 	# disturb_exp()
 
 if __name__ == "__main__":
